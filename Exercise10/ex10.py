08ad439dba144539bc70ffad2ac39fb948398f70
Carmen <carmengagne@att.net>
2018-12-24 14:20:03 -0600

    Exercise 10 done.

---
+++ Exercise10/ex10.py
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat Food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print('BEL \a')
+print('''
+I am triple 
+single quotes
+''')
+print("How does cr \r differ from linefeed \n from formfeed \f?")
+print("vertical tab \v sounds really cool.")
+
